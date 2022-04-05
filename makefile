7acafdcc541c629781167d33fe761f668685c2b8
nout_boo <dunaevv84@gmail.com>
2022-04-05 12:15:22 +0300

    Laba3 almost completed

--- makefile
+++ makefile
@@ -1,8 +1,16 @@
-all: test.o
+all: test utest
+
+test: test.o
 	gcc test.o -o test -fno-pie -no-pie
 
 test.o: test.asm
 	yasm -f elf64 test.asm -o test.o
 
+utest: utest.o
+	gcc utest.o -o utest -fno-pie -no-pie
+
+utest.o: utest.asm
+	yasm -f elf64 utest.asm -o utest.o
+
 clean:
-	rm test.o test
+	rm *test.o *test
---
+++ utest.asm
@@ -0,0 +1,67 @@
+extern printf
+extern scanf
+extern puts
+section .bss
+    ziga dw 0
+    jopa dw 0
+
+section .data
+    msg db "Hello boba", 10, 0
+    frm db "%hi%hi", 0
+    frm2 db "%hi", 10, 0
+
+section .text
+    global main
+
+main:
+    push rbp
+    mov rbp, rsp
+    
+    mov edi, frm
+    mov esi, ziga
+    mov edx, jopa
+    call scanf
+
+    mov ax, [ziga]
+    mov bx, [jopa]
+    cmp ax, bx
+    jl Men
+    je Rav
+    sub ax, bx
+    mov bx, [jopa]
+    xor dx, dx
+    ; cwd
+    div bx 
+
+    mov edi, frm2
+    mov esi, eax
+    call printf
+
+    mov rsp, rbp
+    pop rbp
+    ret
+
+
+Men:
+    xor dx, dx
+    ; cwd
+    div bx
+    add ax, 5
+    mov edi, frm2
+    mov esi, eax
+    call printf
+
+    mov rsp, rbp
+    pop rbp
+    ret
+
+Rav:
+    mov ax, 256
+    mov edi, frm2
+    mov esi, eax
+    call printf
+
+    mov rsp, rbp
+    pop rbp
+    ret
+
